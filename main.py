0e4796a01a513c50aba937485ea11ddd41229021
liarttv <leevi.saastamoinen@gmail.com>
2021-03-25 19:05:16 +0200

    Uploaded base files to git

---
+++ main.py
@@ -0,0 +1,8 @@
+#    This script is made to control audio sources automatically depending on which
+#   scene is selected
+
+# Made by Leevi Saastamoinen (2021)
+
+# This is a free to use plugin
+
+#Git repository:
